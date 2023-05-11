b3e2530117a6319e5010ee116f94ea1382cb8fa2
Alfredo Taboada <alfredotaboada@dhcp-143-18.gm.univ-montp2.fr>
2023-05-11 13:24:30 +0200

    clear

--- js/stress-test.js
+++
@@ -1,9 +0,0 @@
-stress = globalThis['@alfredo-taboada/stress']
-
-
-function test() {
-    const f = new stress.Fault({strike: 45, dipDirection: stress.Direction.SE, dip: 60})
-    f.setStriation({rake: 30, strikeDirection: stress.Direction.NE, sensMouv: stress.SensOfMovement.I})
-
-    console.log(f)
-}
