58fff067301e07e5499e48fc0c329c813b036844
alfredicus <alfredo.taboada@umontpellier.fr>
2024-12-06 11:25:53 +0100

    saving...

--- js/stress-test.js
+++ js/stress-test.js
@@ -2,8 +2,8 @@ stress = globalThis['@alfredo-taboada/stress']
 
 
 function test() {
-    const f = new stress.Fault({strike: 45, dipDirection: stress.Direction.SE, dip: 60})
-    f.setStriation({rake: 30, strikeDirection: stress.Direction.NE, sensMouv: stress.SensOfMovement.I})
+    const f = new stress.Fault({strike: 180, dipDirection: stress.Direction.S, dip: 90})
+    f.setStriation({rake: 30, strikeDirection: stress.Direction.S, typeMov: stress.TypeOfMovement.RL})
 
     console.log(f)
 }
